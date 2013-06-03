0a445cbda8e3d2cc1824f80b0e01844bb9b3532c
Brandon Williams <brandon@kickstarter.com>
2013-06-03 16:25:04 -0400

    Style guide stuff.

--- src/NSObject+OPEnumerable.m
+++ src/NSObject+OPEnumerable.m
@@ -29,214 +29,227 @@
 @implementation NSObject (OPEnumerable)
 
 -(void) each:(void(^)(id obj))iterator {
-    OPAssertEnumerable
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-        iterator(obj);
+  OPAssertEnumerable
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    iterator(obj);
+  }
 }
 
 -(BOOL) all:(BOOL(^)(id obj))block {
-    OPAssertEnumerable
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-        if (! block(obj))
-            return NO;
-    return YES;
+  OPAssertEnumerable
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (! block(obj)) {
+      return NO;
+    }
+  }
+  return YES;
 }
 
 -(BOOL) any:(BOOL(^)(id obj))block {
-    OPAssertEnumerable
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-        if (block(obj))
-            return YES;
-    return NO;
+  OPAssertEnumerable
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (block(obj)) {
+      return YES;
+    }
+  }
+  return NO;
 }
 
 -(BOOL) none:(BOOL(^)(id obj))block {
-    OPAssertEnumerable
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-        if (block(obj))
-            return NO;
-    return YES;
+  OPAssertEnumerable
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (block(obj)) {
+      return NO;
+    }
+  }
+  return YES;
 }
 
 -(BOOL) one:(BOOL(^)(id obj))block {
-    OPAssertEnumerable
-    
-    BOOL retVal = NO;
-    for (id obj in (id<NSFastEnumeration>)self)
-    {
-        if (retVal && (retVal = block(obj)))
-            return NO;
+  OPAssertEnumerable
+
+  BOOL retVal = NO;
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (retVal && (retVal = block(obj))) {
+      return NO;
     }
-    return retVal;
+  }
+  return retVal;
 }
 
 -(id) map:(id(^)(id))mapper {
-    OPAssertEnumerable
-    
-    BOOL isDictionary = OPContainerIsDictionaryLike();
-    id retVal = OPMutableSelfContainer();
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-    {
-        if (isDictionary)
-            [retVal setObject:mapper(obj) forKey:obj];
-        else
-            [retVal addObject:mapper(obj)];
+  OPAssertEnumerable
+
+  BOOL isDictionary = OPContainerIsDictionaryLike();
+  id retVal = OPMutableSelfContainer();
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (isDictionary) {
+      [retVal setObject:mapper(obj) forKey:obj];
+    } else {
+      [retVal addObject:mapper(obj)];
     }
-    return retVal;
+  }
+  return retVal;
 }
 
 -(id) collect:(id(^)(id obj))collector {
-    return [self map:collector];
+  return [self map:collector];
 }
 
 -(id) reduce:(id)initial :(id(^)(id, id))reducer {
-    OPAssertEnumerable
-    
-    id retVal = initial;
-    for (id obj in (id<NSFastEnumeration>)self)
-        retVal = reducer(retVal, obj);
-    return retVal;
+  OPAssertEnumerable
+
+  id retVal = initial;
+  for (id obj in (id<NSFastEnumeration>)self) {
+    retVal = reducer(retVal, obj);
+  }
+  return retVal;
 }
 
 -(int) reducei:(int)initial :(int(^)(int, id))reducer {
-    OPAssertEnumerable
-    
-    int retVal = initial;
-    for (id obj in (id<NSFastEnumeration>)self)
-        retVal = reducer(retVal, obj);
-    return retVal;
+  OPAssertEnumerable
+
+  int retVal = initial;
+  for (id obj in (id<NSFastEnumeration>)self) {
+    retVal = reducer(retVal, obj);
+  }
+  return retVal;
 }
 
 -(float) reducef:(float)initial :(float(^)(float, id))reducer {
-    OPAssertEnumerable
-    
-    float retVal = initial;
-    for (id obj in (id<NSFastEnumeration>)self)
-        retVal = reducer(retVal, obj);
-    return retVal;
+  OPAssertEnumerable
+
+  float retVal = initial;
+  for (id obj in (id<NSFastEnumeration>)self) {
+    retVal = reducer(retVal, obj);
+  }
+  return retVal;
 }
 
 -(double) reduced:(double)initial :(double(^)(double, id))reducer {
-    OPAssertEnumerable
-    
-    double retVal = initial;
-    for (id obj in (id<NSFastEnumeration>)self)
-        retVal = reducer(retVal, obj);
-    return retVal;
+  OPAssertEnumerable
+
+  double retVal = initial;
+  for (id obj in (id<NSFastEnumeration>)self) {
+    retVal = reducer(retVal, obj);
+  }
+  return retVal;
 }
 
 -(id) inject:(id)initial :(id(^)(id sum, id obj))injector {
-    return [self reduce:initial :injector];
+  return [self reduce:initial :injector];
 }
 
 -(int) injecti:(int)initial :(int(^)(int sum, id obj))injector {
-    return [self reducei:initial :injector];
+  return [self reducei:initial :injector];
 }
 
 -(float) injectf:(float)initial :(float(^)(float sum, id obj))injector {
-    return [self reducef:initial :injector];
+  return [self reducef:initial :injector];
 }
 
 -(double) injectd:(double)initial :(double(^)(double sum, id obj))injector {
-    return [self reduced:initial :injector];
+  return [self reduced:initial :injector];
 }
 
 -(id) find:(BOOL(^)(id))finder {
-    OPAssertEnumerable
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-        if (finder(obj))
-            return obj;
-    return nil;
+  OPAssertEnumerable
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (finder(obj)) {
+      return obj;
+    }
+  }
+  return nil;
 }
 
 -(id) detect:(BOOL (^)(id))detector {
-    return [self find:detector];
+  return [self find:detector];
 }
 
 -(id) findAll:(BOOL(^)(id))finder {
-    OPAssertEnumerable
-    
-    BOOL isDictionary = OPContainerIsDictionaryLike();
-    id retVal = OPMutableSelfContainer();
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-    {
-        if (finder(obj))
-        {
-            if (isDictionary)
-                [retVal setObject:obj forKey:obj];
-            else
-                [retVal addObject:obj];
-        }
+  OPAssertEnumerable
+
+  BOOL isDictionary = OPContainerIsDictionaryLike();
+  id retVal = OPMutableSelfContainer();
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (finder(obj)) {
+      if (isDictionary) {
+        [retVal setObject:obj forKey:obj];
+      } else {
+        [retVal addObject:obj];
+      }
     }
-    return retVal;
+  }
+  return retVal;
 }
 
 -(id) reject:(BOOL(^)(id obj))rejector {
-    return [self findAll:^BOOL(id obj) {
-        return ! rejector(obj);
-    }];
+  return [self findAll:^BOOL(id obj) {
+    return ! rejector(obj);
+  }];
 }
 
 -(id) compact {
-    OPAssertEnumerable
-    
-    BOOL isDictionary = OPContainerIsDictionaryLike();
-    id retVal = [self mutableCopy];
-    
-    for (id obj in (id<NSFastEnumeration>)self)
-    {
-        if (isDictionary && [retVal objectForKey:obj] == [NSNull null])
-            [retVal removeObjectForKey:obj];
-        else if (! isDictionary && obj == [NSNull null])
-            [retVal removeObjectForKey:obj];
+  OPAssertEnumerable
+
+  BOOL isDictionary = OPContainerIsDictionaryLike();
+  id retVal = [self mutableCopy];
+
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (isDictionary && [retVal objectForKey:obj] == [NSNull null]) {
+      [retVal removeObjectForKey:obj];
+    } else if (! isDictionary && obj == [NSNull null]) {
+      [retVal removeObjectForKey:obj];
     }
-    return retVal;
+  }
+  return retVal;
 }
 
 -(id) max {
-    OPAssertEnumerable
-    
-    BOOL isDictionary = OPContainerIsDictionaryLike();
-    id retVal = [self find:^BOOL(id obj) { return YES; }];
-    for (id obj in (id<NSFastEnumeration>)self) {
-        if (isDictionary && [[(id)self objectForKey:obj] compare:retVal] == NSOrderedDescending) {
-            retVal = [(id)self objectForKey:obj];
-        } else if (! isDictionary && [obj compare:retVal] == NSOrderedDescending) {
-            retVal = obj;
-        }
+  OPAssertEnumerable
+
+  BOOL isDictionary = OPContainerIsDictionaryLike();
+  id retVal = [self find:^BOOL(id obj) { return YES; }];
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (isDictionary && [[(id)self objectForKey:obj] compare:retVal] == NSOrderedDescending) {
+      retVal = [(id)self objectForKey:obj];
+    } else if (! isDictionary && [obj compare:retVal] == NSOrderedDescending) {
+      retVal = obj;
     }
-    return retVal;
+  }
+  return retVal;
 }
 
 -(id) min {
-    OPAssertEnumerable
-    
-    BOOL isDictionary = OPContainerIsDictionaryLike();
-    id retVal = [self find:^BOOL(id obj) { return YES; }];
-    for (id obj in (id<NSFastEnumeration>)self) {
-        if (isDictionary && [[(id)self objectForKey:obj] compare:retVal] == NSOrderedAscending) {
-            retVal = [(id)self objectForKey:obj];
-        } else if (! isDictionary && [obj compare:retVal] == NSOrderedAscending) {
-            retVal = obj;
-        }
+  OPAssertEnumerable
+
+  BOOL isDictionary = OPContainerIsDictionaryLike();
+  id retVal = [self find:^BOOL(id obj) { return YES; }];
+  for (id obj in (id<NSFastEnumeration>)self) {
+    if (isDictionary && [[(id)self objectForKey:obj] compare:retVal] == NSOrderedAscending) {
+      retVal = [(id)self objectForKey:obj];
+    } else if (! isDictionary && [obj compare:retVal] == NSOrderedAscending) {
+      retVal = obj;
     }
-    return retVal;
+  }
+  return retVal;
 }
 
 -(NSDictionary*) partition:(BOOL(^)(id obj))partitioner {
-    OPAssertEnumerable
-    
-    NSMutableDictionary *retVal = [@{ @(YES) : [@[] mutableCopy], @(NO) : [@[] mutableCopy] } mutableCopy];
-    for (id obj in (id<NSFastEnumeration>)self)
-        [[retVal objectForKey:@(partitioner(obj))] addObject:obj];
-    return retVal;
+  OPAssertEnumerable
+
+  NSMutableDictionary *retVal = [@{ @(YES) : [@[] mutableCopy], @(NO) : [@[] mutableCopy] } mutableCopy];
+  for (id obj in (id<NSFastEnumeration>)self) {
+    [[retVal objectForKey:@(partitioner(obj))] addObject:obj];
+  }
+  return retVal;
 }
 
 @end
